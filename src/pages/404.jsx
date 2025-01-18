20e4805d8db18493e14067279a783d83471fbeb5
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-01-18 19:21:57 +0330

    create and add style to 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,22 @@
+// import React from "react";
+
+// function PageNotFound() {
+//   return <div>PageNotFound</div>;
+// }
+
+// export default PageNotFound;
 import React from "react";
+import '../styles/PageNotFound.css';  // فایل CSS را برای استایل دهی اضافه کنید
 
 function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="page-not-found">
+      <img src="./404-error.svg" alt="Page Not Found" />
+      <h1>اوه! صفحه‌ای که دنبال آن بودید یافت نشد.</h1>
+      <p>متاسفانه صفحه مورد نظر شما پیدا نشد. شاید بخواهید به صفحه اصلی برگردید.</p>
+      <a href="/" className="home-link">بازگشت به صفحه اصلی</a>
+    </div>
+  );
 }
 
 export default PageNotFound;
