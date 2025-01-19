c264f02977e5f1771ee3d47ad78cae225fdf74f7
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-01-19 19:09:34 +0330

    add delete button and styles to categoris

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -1,21 +1,32 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCategory } from "../../services/admin";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getCategory, deleteCategory } from "../../services/admin";
 import Loader from "../modules/Loader";
+import styles from "./CategoryList.module.css";
 
 function CategoryList() {
+  const queryClient = useQueryClient();
   const { data, isLoading } = useQuery(["get-categoris"], getCategory);
-  console.log({ data, isLoading });
+  const deleteMutation = useMutation(deleteCategory, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["get-categoris"]);
+    },
+  });
+
+  const handleDelete = (id) => {
+    deleteMutation.mutate(id);
+  };
 
   return (
-    <div>
+    <div className={styles.list}>
       {isLoading ? (
         <Loader />
       ) : (
         data.data.map((i) => (
-          <div key={i._id}>
-            <img src={`${i.icon}.svg`} />
+          <div key={i._id} className={styles.card}>
+            <img src={`${i.icon}.svg`} alt={`${i.name} icon`} />
             <h5>{i.name}</h5>
-            <p>slug:{i.slug}</p>
+            <p>slug: {i.slug}</p>
+            <button onClick={() => handleDelete(i._id)}>حذف</button>
           </div>
         ))
       )}
@@ -24,3 +35,31 @@ function CategoryList() {
 }
 
 export default CategoryList;
+
+// import { useQuery } from "@tanstack/react-query";
+// import { getCategory } from "../../services/admin";
+// import Loader from "../modules/Loader";
+// import styles from "./CategoryList.module.css";
+
+// function CategoryList() {
+//   const { data, isLoading } = useQuery(["get-categoris"], getCategory);
+//   console.log({ data, isLoading });
+
+//   return (
+//     <div className={styles.list}>
+//       {isLoading ? (
+//         <Loader />
+//       ) : (
+//         data.data.map((i) => (
+//           <div key={i._id}>
+//             <img src={`${i.icon}.svg`} />
+//             <h5>{i.name}</h5>
+//             <p>slug:{i.slug}</p>
+//           </div>
+//         ))
+//       )}
+//     </div>
+//   );
+// }
+
+// export default CategoryList;
