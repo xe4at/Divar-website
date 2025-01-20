98befa9f7be31c58b7f4a2aa5009898b8f441f31
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-01-20 19:18:57 +0330

    send file and image to backend

--- src/components/templates/AddPost.jsx
+++ src/components/templates/AddPost.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import { getCategory } from "../../services/admin";
+import { getCookie } from "../../utils/cookies";
 
 import styles from "./AddPost.module.css";
+import axios from "axios";
 
 function AddPost() {
   const [form, setForm] = useState({
@@ -28,7 +30,22 @@ function AddPost() {
 
   const addHandler = (event) => {
     event.preventDefault();
-    console.log(form);
+
+    const formData = new FormData();
+    for (let i in form) {
+      formData.append(i, form[i]);
+    }
+    const token = getCookie("accessToken");
+
+    axios
+      .post(`${import.meta.env.VITE_BASE_URL}/post/create`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `bearer ${token}`,
+        },
+      })
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -39,7 +56,7 @@ function AddPost() {
       <label htmlFor="content">توضیحات</label>
       <textarea name="content" id="content" />
       <label htmlFor="amount">قیمت</label>
-      <input type="text" name="amount" id="amount" />
+      <input type="number" name="amount" id="amount" />
       <label htmlFor="city">شهر</label>
       <input type="text" name="city" id="city" />
       <label htmlFor="category">دسته بندی</label>
