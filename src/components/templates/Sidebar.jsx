8d22d391188694c7fcdec79e6a5796d16abe90ef
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-01-24 15:16:24 +0330

    create sidebar and add style

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,5 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCategory } from "../../services/admin";
+
+import styles from "./Sidebar.module.css";
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const { data } = useQuery(["get-categories"], getCategory);
+
+  return (
+    <div className={styles.sidebar}>
+      <h4>دسته بندی ها</h4>
+      <ul>
+        {data?.data.map((category) => {
+          return (
+            <li key={category._id}>
+              <img src={`${category.icon}.svg`} />
+              <p>{category.name}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
