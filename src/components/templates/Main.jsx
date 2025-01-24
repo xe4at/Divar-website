b26e178552440965c791d7f9d6d0fe23f963fcea
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-01-24 15:53:05 +0330

    Show all ads

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,5 +1,23 @@
-function Main() {
-  return <div>Main</div>;
+import { sp } from "../../utils/numbers";
+
+function Main({ posts }) {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  return (
+    <div>
+      {posts.data.posts.map((post) => (
+        <div key={post._id}>
+          <div>
+            <p>{post.options.title}</p>
+            <div>
+              <p>{sp(post.amount)}</p>
+              <span>{post.options.city}</span>
+            </div>
+          </div>
+          <img src={`${baseUrl}${post.images[0]}`} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Main;
